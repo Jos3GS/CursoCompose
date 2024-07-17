fa210223bec17a288c2ade758dfa5720630c7638
Jos3GS <gutierrez.sossa.j@gmail.com>
2024-07-16 20:05:37 -0500

    usando modificadores

--- app/src/main/java/com/example/mynewcompose/ui/MainActivity.kt
+++ app/src/main/java/com/example/mynewcompose/ui/MainActivity.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mynewcompose.ui.theme.MyNewComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,8 +34,6 @@ class MainActivity : ComponentActivity() {
 
 @Preview(
     name = "My Super Text",
-    heightDp = 50,
-    widthDp = 200,
     showBackground = true,
     showSystemUi = true,
     apiLevel = 33,
@@ -45,7 +47,10 @@ fun MyTestSuperText() {
 
 @Composable
 fun MySuperText(name: String) {
-    Text(text = "SOY $name :D")
+    Text(text = "SOY $name :D", modifier = Modifier
+        .fillMaxSize()
+        .padding(horizontal =16.dp)
+        .clickable { } )
 }
 
 
