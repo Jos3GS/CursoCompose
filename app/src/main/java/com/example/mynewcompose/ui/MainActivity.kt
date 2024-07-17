8185a6ead17363229aeb370c37d550cb95de2f9f
Jos3GS <gutierrez.sossa.j@gmail.com>
2024-07-16 19:55:53 -0500

    Usando Previews

--- app/src/main/java/com/example/mynewcompose/ui/MainActivity.kt
+++ app/src/main/java/com/example/mynewcompose/ui/MainActivity.kt
@@ -7,8 +7,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mynewcompose.ui.theme.MyNewComposeTheme
 
@@ -18,20 +20,35 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyNewComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    MySuperText("JOSESITO")
                 }
             }
         }
     }
 }
 
+@Preview(
+    name = "My Super Text",
+    heightDp = 50,
+    widthDp = 200,
+    showBackground = true,
+    showSystemUi = true,
+    apiLevel = 33,
+    device = Devices.NEXUS_5
+)
+@Composable
+fun MyTestSuperText() {
+    MySuperText("JOSEEE")
+}
 
 
-@Preview(showBackground = true)
+@Composable
+fun MySuperText(name: String) {
+    Text(text = "SOY $name :D")
+}
+
+
 @Composable
 fun GreetingPreview() {
     MyNewComposeTheme {
